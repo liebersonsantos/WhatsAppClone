89bdf921685064465cda8203585b4a980d0233d3
liebersonsantos <liebersonsantos.one@gmail.com>
2018-03-08 19:48:25 -0300

    inclusão de Toolbar e alterações no style.xml

--- app/src/main/java/com/example/lieberson/whatsappclone/activity/LoginActivity.java
+++ app/src/main/java/com/example/lieberson/whatsappclone/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,12 +17,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.lieberson.whatsappclone.Model.Usuario;
 import com.example.lieberson.whatsappclone.R;
 import com.example.lieberson.whatsappclone.config.ConfiguracaoFirebase;
 import com.example.lieberson.whatsappclone.helper.Permissao;
 import com.example.lieberson.whatsappclone.helper.Preferencias;
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 
 import java.util.HashMap;
@@ -31,26 +37,87 @@ public class LoginActivity extends AppCompatActivity {
 
     private DatabaseReference referenciaFirebase;
 
+    private EditText email;
+    private EditText senha;
+    private Button botaoLogar;
+
+    private Usuario usuario;
+
+    private FirebaseAuth autenticacao;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        verificarUsuarioLogado();
 
-        referenciaFirebase = ConfiguracaoFirebase.getFirebase();
-        referenciaFirebase.child("pontos").setValue(800);
+        email = findViewById(R.id.edit_email);
+        senha = findViewById(R.id.edit_senha);
+        botaoLogar = findViewById(R.id.bt_logar);
+
+        botaoLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                usuario = new Usuario();
+                usuario.setEmail(email.getText().toString());
+                usuario.setSenha(senha.getText().toString());
+
+                validarLogin(); // Método responsável por fazer a validação do usuário
+            }
+        });
+
+    }
+
+    public void validarLogin(){
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+
+                            Toast.makeText(LoginActivity.this, "Sucesso ao fazer Login", Toast.LENGTH_SHORT).show();
+                            abrirTelaPrincipal();
+
+                        }else {
+
+                            Toast.makeText(LoginActivity.this, "Erro ai fazer Login", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+    }
+
+    private void verificarUsuarioLogado(){
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
 
+        if (autenticacao.getCurrentUser() != null){//recupera o usuario atual
+
+            abrirTelaPrincipal();
+        }
     }
 
     public void abrirCadastroUsuario(View view){
 
         Intent intent = new Intent(LoginActivity.this, CadastroUsuarioActivity.class);
         startActivity(intent);
+    }
 
+    private void abrirTelaPrincipal(){
 
-
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 
--- app/src/main/java/com/example/lieberson/whatsappclone/activity/MainActivity.java
+++ app/src/main/java/com/example/lieberson/whatsappclone/activity/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.lieberson.whatsappclone.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.lieberson.whatsappclone.R;
 import com.example.lieberson.whatsappclone.config.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private FirebaseAuth usuarioAutenticacao;
 
 
     @Override
@@ -16,18 +27,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        usuarioAutenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        toolbar = findViewById(R.id.toolbar_principal);
+        toolbar.setTitle("WhatsApp");
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater(); //cria um objeto ja com o contexto da nossa aplicaçao
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.item_sair:
+                deslogarUsuario();
+                return true;
+            case R.id.item_configuracoes:
+                return true;
+            default: return super.onOptionsItemSelected(item);
+
+        }
 
 
 
 
+    }
 
+    private void deslogarUsuario() {
 
+        usuarioAutenticacao.signOut();
 
-
-
-
-
-
-
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
